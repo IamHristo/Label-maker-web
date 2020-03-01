8da0a4919b28f82878c85ab0949e4a55fbbd1b61
IceMaDaFaKa <56919704+IceMaDaFaKa@users.noreply.github.com>
2020-03-01 19:45:29 +0200

    test

--- mt.js
+++ mt.js
@@ -14,3 +14,15 @@ function genPDF() {
 	doc.save('Test.pdf');
 	
 }
+
+function printPDF(){
+    //Get the print button and put it into a variable
+        var printButton = document.getElementById("btn");
+        //Set the print button visibility to 'hidden' 
+        printButton.style.visibility = 'hidden';
+        //Print the page content
+        window.print()
+        //Set the print button to 'visible' again 
+        //[Delete this line if you want it to stay hidden after printing]
+        printButton.style.visibility = 'visible';
+}
